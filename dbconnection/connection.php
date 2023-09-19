dfd0e54c6c4616b0a647712a87413918ba0ee6d4
jackon shiundu <jackonshiundu2019@gmail.com>
2023-09-19 09:59:00 +0300

    dbchanges

--- dbconnection/connection.php
+++ dbconnection/connection.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
 define('BR','<br>');
- $db = mysqli_connect('localhost', 'root', 'Jack_2000.ja@23','windandscreening') or
+ $db = mysqli_connect('localhost', 'root', 'password','dbname') or
         die ('Unable to connect. Check your connection parameters.');
 ?>
